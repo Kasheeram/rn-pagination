34fcc87c77d0d4d5945b4d9b303b9a60d7a97ffd
Kashee Ram Kushwaha <kashee.nitd2016@gmail.com>
2020-11-03 13:18:38 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator, } from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [dataSource, setDataSource] = useState([]);
+  const [offset, setOffset] = useState(1);
+
+  useEffect(() => getData(), []);
+
+  const getData = () => {
+    console.log('getData');
+    setLoading(true);
+    //Service to get the data from the server to render
+    fetch('https://aboutreact.herokuapp.com/getpost.php?offset='
+          + offset)
+      //Sending the currect offset with get request
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //Successful response
+        setOffset(offset + 1);
+        //Increasing the offset for the next API call
+        setDataSource([...dataSource, ...responseJson.results]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const renderFooter = () => {
+    return (
+      //Footer View with Load More button
+      <View style={styles.footer}>
+          <Text style={styles.btnText}>Load More</Text>
+          {loading ? (
+            <ActivityIndicator
+              color="gray"
+              size="small"
+              style={{alignItems: 'center'}} />
+          ) : null}
+
+      </View>
+    );
+  };
+
+  const ItemView = ({item}) => {
+    return (
+      // Flat List Item
+      <Text
+        style={styles.itemStyle}
+        onPress={() => getItem(item)}>
+        {item.id}
+        {'. '}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    //Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <FlatList
+          data={dataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          enableEmptySections={true}
+          renderItem={ItemView}
+          ListFooterComponent={renderFooter}
+          onEndReachedThreshold={0}
+          onEndReached={getData}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
+export default App;
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+  },
+  itemStyle: {
+    marginVertical:10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  footer: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadMoreBtn: {
+    padding: 10,
+    backgroundColor: '#800000',
+    borderRadius: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
